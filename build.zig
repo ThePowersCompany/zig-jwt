f9160dff9cc56f0e3518e0797563127dd2490bc5
Bradley Odell <btodell@hotmail.com>
2026-01-07 12:31:53 -0900

    Improve build script

--- build.zig
+++ build.zig
@@ -2,10 +2,12 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
 
     const mod = b.addModule("zig-jwt", .{
         .root_source_file = b.path("src/jwt.zig"),
         .target = target,
+        .optimize = optimize,
     });
 
     // -Dtest-filter="..."
